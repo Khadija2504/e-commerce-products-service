8b630488c9eac5290c29466ccdaf55373fe5bd95
Khadija2504 <khadijalady25@gmail.com>
2025-02-19 12:07:02 +0100

    EC-13 update the pom to fix the excution and the redirected issues

--- src/main/java/com/e_commerce/product/ProductApplication.java
+++ src/main/java/com/e_commerce/product/ProductApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.e_commerce.product"})
-//@MapperScan("com.e_commerce.product.mapper")
 public class ProductApplication {
 
 	public static void main(String[] args) {
