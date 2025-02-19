585c4df1a921aed64ee02f0c16b4fa37389789ec
Khadija2504 <khadijalady25@gmail.com>
2025-02-19 11:54:24 +0100

    EC-13 add the product mapper and the controller with the service impl logic for the add product methods, update the pom to fix a mapper issue

--- src/main/java/com/e_commerce/product/ProductApplication.java
+++ src/main/java/com/e_commerce/product/ProductApplication.java
@@ -3,7 +3,8 @@ package com.e_commerce.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.e_commerce.product"})
+//@MapperScan("com.e_commerce.product.mapper")
 public class ProductApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/e_commerce/product/service/impl/ProductServiceImpl.java
+++ src/main/java/com/e_commerce/product/service/impl/ProductServiceImpl.java
@@ -1,8 +1,17 @@
 package com.e_commerce.product.service.impl;
 
+import com.e_commerce.product.model.Product;
+import com.e_commerce.product.repository.ProductRepository;
 import com.e_commerce.product.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+    @Override
+    public Product addProduct(Product product) {
+        return productRepository.save(product);
+    }
 }
